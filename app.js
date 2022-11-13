752716a2a4e499214ddd651436bbc361dc407f42
Eyup <117532693+eyupeminoglu@users.noreply.github.com>
2022-11-13 22:49:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running and listening on port ${port}/`);
 });
